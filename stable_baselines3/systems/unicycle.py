fixed a bug in reward computation

stable_baselines3/systems/unicycle.py
@@ -160,7 +160,7 @@ class Unicycle(gym.Env):
 		return np.float32(obs)
 	
 	def _get_cost(self, action, state_):
-		x = self.get_obs()
+		x = self.get_obs(normalized=False)
 		x = x[:,np.newaxis]
 		a = action[:,np.newaxis]
 