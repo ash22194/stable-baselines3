9a9f974f62fb9fed5a29552b9f66c468bbf23c37
Ashwin Khadke <ashwinkhadke@ashwins-air.wifi.local.cmu.edu>
2023-12-11 16:19:47 -0500

    modified the unicycle test script

--- examples/testUnicycle.py
+++ examples/testUnicycle.py
@@ -6,16 +6,16 @@ from copy import deepcopy
 import gymnasium as gym
 from cProfile import Profile
 import time
+from ipdb import set_trace
 
 from stable_baselines3.common.env_checker import check_env
 
 def main():
 	normalized_actions = True
-	fixed_start = False
-	expand_limits = False
-	env_kwargs = dict(fixed_start=fixed_start, normalized_actions=normalized_actions, expand_limits=expand_limits)
+	fixed_start = True
+	env_kwargs = dict(fixed_start=fixed_start, normalized_actions=normalized_actions)
 	env = gym.make('Unicycle-v0', **env_kwargs)
-	check_env(env)
+	# check_env(env)
 
 	# Test dynamics
 	prof = Profile()
@@ -25,14 +25,15 @@ def main():
 	dyn_time = 0
 	dyn_ana_time = 0
 	for nn in range(num_samples):
-		state, _ = env.reset()
+		env.reset()
+		state = env.state
 		inp = 0.5 * (env.u_limits[:,1] + env.u_limits[:,0]) + (np.random.rand(env.U_DIMS) - 0.5) * (env.u_limits[:,1] - env.u_limits[:,0])
 		start_dyn = time.process_time()
-		dx = env.dyn(state[:,np.newaxis], inp[:,np.newaxis])
+		dx = env.dyn_full(state[:,np.newaxis], inp[:,np.newaxis])
 		dyn_time += (time.process_time() - start_dyn)
 
 		start_dyn_ana = time.process_time()
-		dx_ana = env.dyn_ana(state[:,np.newaxis], inp[:,np.newaxis])
+		dx_ana = env.dyn_full_ana(state[:,np.newaxis], inp[:,np.newaxis])
 		dyn_ana_time += (time.process_time() - start_dyn_ana)
 
 		dx_errs[nn] = np.linalg.norm(dx - dx_ana)
@@ -44,35 +45,36 @@ def main():
 	prof.create_stats()
 	prof.dump_stats('unicycle_profile')
 	goal = env.goal
+	x0 = np.zeros((env.X_DIMS, 1))
+	x0[env.observation_dims,:] = goal
 	u0 = env.u0
 	A = np.zeros((env.X_DIMS, env.X_DIMS))
 	B = np.zeros((env.X_DIMS, env.U_DIMS))
 	eps = 1e-6
 	for xx in range(env.X_DIMS):
-		perturb_p = np.zeros(goal.shape)
-		perturb_p[xx] = eps
-		perturb_m = np.zeros(goal.shape)
-		perturb_m[xx] = -eps
-		dyn_p = env.dyn(goal + perturb_p, u0)
-		dyn_m = env.dyn(goal + perturb_m, u0)
+		perturb = np.zeros((env.X_DIMS, 1))
+		perturb[xx, 0] = eps
+		dyn_p = env.dyn_full_ana(x0 + perturb, u0)
+		dyn_m = env.dyn_full_ana(x0 - perturb, u0)
 
 		A[:, xx:(xx+1)] = (dyn_p - dyn_m) / (2*eps)
-	
+
 	for uu in range(env.U_DIMS):
-		perturb_p = np.zeros(u0.shape)
-		perturb_p[uu] = eps
-		perturb_m = np.zeros(u0.shape)
-		perturb_m[uu] = -eps
-		dyn_p = env.dyn(goal, u0 + perturb_p)
-		dyn_m = env.dyn(goal, u0 + perturb_m)
+		perturb = np.zeros(u0.shape)
+		perturb[uu] = eps
+		dyn_p = env.dyn_full_ana(x0, u0 + perturb)
+		dyn_m = env.dyn_full_ana(x0, u0 - perturb)
 
 		B[:, uu:(uu+1)] = (dyn_p - dyn_m) / (2*eps)
+	A = A[:,env.observation_dims]
+	A = A[env.observation_dims,:]
+	B = B[env.observation_dims,:]
 
 	Q = env.Q
 	R = env.R
 	lambda_ = 0.5
 	gamma_ = 0.9995
-	P = solve_continuous_are(A - lambda_/2*np.eye(env.X_DIMS), B, Q, R)
+	P = solve_continuous_are(A - lambda_/2*np.eye(A.shape[0]), B, Q, R)
 	K = np.matmul(np.linalg.inv(R), np.matmul(B.T, P))
 
 	with np.printoptions(precision=3, suppress=True):
@@ -83,26 +85,67 @@ def main():
 		print('LQR gain :', K)
 
 	# Test the policy
-	obs, _ = env.reset()
-	start = obs
+	err_p = np.zeros((3, int(env.T / env.dt)))
+	err_v = np.zeros((3, int(env.T / env.dt)))
+	err_a = np.zeros((3, int(env.T / env.dt)))
+	obs, info = env.reset()
+	env.render()
+	set_trace()
+	start = env.state
 	u_limits = env.u_limits
 	value = 0
 	discount = 1
 	for i in range(int(env.T / env.dt)):
 		action = np.matmul(-K, obs[:,np.newaxis] - goal)[:,0]
 		action = np.maximum(u_limits[:,0], np.minimum(u_limits[:,1], action))
+		with np.printoptions(precision=3, suppress=True):
+			print('u :', action)
+			print('v :', env.state[8:])
+
 		if (normalized_actions):
 			action = (2*action - (u_limits[:,1] + u_limits[:,0])) / (u_limits[:,1] - u_limits[:,0])
+		
+		with np.printoptions(precision=3, suppress=True):
+			print('dv :', env.dyn_full(env.state[:,np.newaxis], action[:,np.newaxis])[8:,0])
 
 		obs, reward, done, _, info = env.step(action)
+		env.render()
+		err_p[:,i] = deepcopy(info['contact_info']['err_p'])
+		err_v[:,i] = deepcopy(info['contact_info']['err_v'])
+		err_a[:,i] = deepcopy(info['contact_info']['err_a'])
+		with np.printoptions(precision=3, suppress=True):
+			print('e_p :', err_p[:,i])
+			print('e_v :', err_v[:,i])
+			print('e_a :', err_a[:,i])
+
 		value += (discount*reward)
 		discount *= gamma_
-
+		# set_trace()
 		if done:
-			print('Start state :', start, ', Final state :', obs, 'Value :', value)
-			obs, _ = env.reset()
-			start = obs
-			value = 0
+			err_p = err_p[:,:i]
+			err_v = err_v[:,:i]
+			err_a = err_a[:,:i]
+			print('Start state :', start)
+			print('Final state :', env.state)
+			print('Value :', value)
+			break
 
+	fig, ax = plt.subplots(3)
+	time_steps = np.linspace(0, i*env.dt, i)
+	ax[0].plot(time_steps, err_p[0,:], color='r')
+	ax[0].plot(time_steps, err_p[1,:], color='g')
+	ax[0].plot(time_steps, err_p[2,:], color='b')
+
+	ax[1].plot(time_steps, err_v[0,:], color='r')
+	ax[1].plot(time_steps, err_v[1,:], color='g')
+	ax[1].plot(time_steps, err_v[2,:], color='b')
+
+	ax[2].plot(time_steps, err_a[0,:], color='r')
+	ax[2].plot(time_steps, err_a[1,:], color='g')
+	ax[2].plot(time_steps, err_a[2,:], color='b')
+
+	plt.show()
+
+	set_trace()
 if __name__=='__main__':
 	main()
