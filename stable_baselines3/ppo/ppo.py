c47c370d2a8b25f86fed9898b0baf360e7b95ba4
Ashwin Khadke <akhadke@br012.ib.bridges2.psc.edu>
2024-02-04 02:02:12 -0500

    Added the functionality to have modular policies

--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -7,7 +7,7 @@ from gymnasium import spaces
 from torch.nn import functional as F
 
 from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
-from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
+from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, ModularActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import explained_variance, get_schedule_fn, update_learning_rate
 from stable_baselines3.common.buffers import RolloutBuffer
@@ -71,6 +71,7 @@ class PPO(OnPolicyAlgorithm):
 
     policy_aliases: ClassVar[Dict[str, Type[BasePolicy]]] = {
         "MlpPolicy": ActorCriticPolicy,
+        "ModularMlpPolicy": ModularActorCriticPolicy,
         "CnnPolicy": ActorCriticCnnPolicy,
         "MultiInputPolicy": MultiInputActorCriticPolicy,
     }
